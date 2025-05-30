71f3bdf8d4e4c19f6831d3d99f21e05f3d0ffecc
HYAORKFY <152956209+HYAORKFY@users.noreply.github.com>
2025-05-30 15:45:15 +0800

    Add the retransmission mechanism of the client side
    The client sets a timeout after sending the request. If no response is received, the request will be retransmitted.

--- client/UDPclient.java
+++ client/UDPclient.java
@@ -4,6 +4,7 @@ import java.util.Base64;
 
 public class UDPclient {
     private static final int INITIAL_TIMEOUT = 1000;
+    private static final int MAX_RETRY = 5;
 
     public static void main(String[] args) throws IOException {
         if (args.length != 3) {
@@ -29,43 +30,55 @@ public class UDPclient {
             String request = "DOWNLOAD " + filename;
             byte[] sendData = request.getBytes();
             DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, address, serverPort);
-            socket.send(sendPacket);
 
-            byte[] receiveData = new byte[1024];
-            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-            socket.receive(receivePacket);
-            String response = new String(receivePacket.getData(), 0, receivePacket.getLength()).trim();
+            boolean receivedResponse = false;
+            int attempt = 0;
+            while (!receivedResponse && attempt < MAX_RETRY) {
+                try {
+                    socket.send(sendPacket);
+                    byte[] receiveData = new byte[1024];
+                    DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                    socket.setSoTimeout(INITIAL_TIMEOUT);
+                    socket.receive(receivePacket);
+                    String response = new String(receivePacket.getData(), 0, receivePacket.getLength()).trim();
+                    receivedResponse = true;
+                    System.out.println("Server response: " + response);
 
-            if (response.startsWith("ERR")) {
-                System.out.println("Error: " + response);
-                return;
-            }
+                    if (response.startsWith("ERR")) {
+                        System.out.println("Error: " + response);
+                        return;
+                    }
 
-            String[] okParts = response.split(" ");
-            long fileSize = Long.parseLong(okParts[3]);
-            int dataPort = Integer.parseInt(okParts[5]);
-            System.out.println("Downloading " + filename + " (" + fileSize + " bytes)");
+                    String[] okParts = response.split(" ");
+                    long fileSize = Long.parseLong(okParts[3]);
+                    int dataPort = Integer.parseInt(okParts[5]);
+                    System.out.println("Downloading " + filename + " (" + fileSize + " bytes)");
 
-            try (RandomAccessFile file = new RandomAccessFile(filename, "rw")) {
-                int current = 0;
-                while (current < fileSize) {
-                    int end = (int) Math.min(current + 999, fileSize - 1);
-                    String dataRequest = String.format("FILE %s GET START %d END %d", filename, current, end);
-                    byte[] dataSend = dataRequest.getBytes();
-                    DatagramPacket dataSendPacket = new DatagramPacket(dataSend, dataSend.length, address, dataPort);
-                    socket.send(dataSendPacket);
+                    try (RandomAccessFile file = new RandomAccessFile(filename, "rw")) {
+                        int current = 0;
+                        while (current < fileSize) {
+                            int end = (int) Math.min(current + 999, fileSize - 1);
+                            String dataRequest = String.format("FILE %s GET START %d END %d", filename, current, end);
+                            byte[] dataSend = dataRequest.getBytes();
+                            DatagramPacket dataSendPacket = new DatagramPacket(dataSend, dataSend.length, address, dataPort);
+                            socket.send(dataSendPacket);
 
-                    byte[] dataReceive = new byte[2048];
-                    DatagramPacket dataReceivePacket = new DatagramPacket(dataReceive, dataReceive.length);
-                    socket.receive(dataReceivePacket);
-                    String dataResponse = new String(dataReceivePacket.getData(), 0, dataReceivePacket.getLength()).trim();
+                            byte[] dataReceive = new byte[2048];
+                            DatagramPacket dataReceivePacket = new DatagramPacket(dataReceive, dataReceive.length);
+                            socket.receive(dataReceivePacket);
+                            String dataResponse = new String(dataReceivePacket.getData(), 0, dataReceivePacket.getLength()).trim();
 
-                    String[] parts = dataResponse.split(" DATA ");
-                    byte[] decodedData = Base64.getDecoder().decode(parts[1]);
-                    file.seek(current);
-                    file.write(decodedData);
-                    current += decodedData.length;
-                    System.out.print("*");
+                            String[] parts = dataResponse.split(" DATA ");
+                            byte[] decodedData = Base64.getDecoder().decode(parts[1]);
+                            file.seek(current);
+                            file.write(decodedData);
+                            current += decodedData.length;
+                            System.out.print("*");
+                        }
+                    }
+                } catch (SocketTimeoutException e) {
+                    attempt++;
+                    System.out.println("Timeout, retrying... (" + attempt + "/" + MAX_RETRY + ")");
                 }
             }
         } catch (IOException e) {
