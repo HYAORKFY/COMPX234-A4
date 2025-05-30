dd724b3891ba312d429d5333eeee635bc972206a
HYAORKFY <152956209+HYAORKFY@users.noreply.github.com>
2025-05-30 15:43:18 +0800

    Implement the server logic for file data transmission
    The server receives the "FILE <filename> GET START <start> END <end>" request from the client and sends the corresponding file data. java

--- server/UDPserver.java
+++ server/UDPserver.java
@@ -1,7 +1,10 @@
 import java.io.*;
 import java.net.*;
+import java.util.Base64;
 
 public class UDPserver {
+    private static final int MAX_RETRY = 5;
+
     public static void main(String[] args) throws IOException {
         if (args.length != 1) {
             System.err.println("Usage: java UDPserver <port>");
@@ -32,15 +35,50 @@ public class UDPserver {
                     if (!file.exists()) {
                         String response = "ERR " + filename + " NOT_FOUND";
                         sendResponse(welcomeSocket, clientAddress, clientPort, response);
-                    } else {
-                        int dataPort = 50000 + (int) (Math.random() * 1000);
-                        String okResponse = "OK " + filename + " SIZE " + file.length() + " PORT " + dataPort;
-                        sendResponse(welcomeSocket, clientAddress, clientPort, okResponse);
+                        return;
                     }
+
+                    int dataPort = 50000 + (int) (Math.random() * 1000);
+                    String okResponse = "OK " + filename + " SIZE " + file.length() + " PORT " + dataPort;
+                    sendResponse(welcomeSocket, clientAddress, clientPort, okResponse);
+
+                    DatagramSocket dataSocket = new DatagramSocket(dataPort);
+                    handleFileTransfer(dataSocket, file, clientAddress, clientPort);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }).start();
+        }
+    }
+
+    private static void handleFileTransfer(DatagramSocket dataSocket, File file, InetAddress clientAddress, int clientPort) {
+        try (RandomAccessFile raf = new RandomAccessFile(file, "r")) {
+            byte[] buffer = new byte[1024];
+            while (true) {
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                dataSocket.receive(packet);
+                String request = new String(packet.getData(), 0, packet.getLength()).trim();
+                String[] tokens = request.split(" ");
+
+                if (tokens[0].equals("FILE") && tokens[2].equals("CLOSE")) {
+                    String response = "FILE " + tokens[1] + " CLOSE_OK";
+                    sendResponse(dataSocket, clientAddress, clientPort, response);
+                    break;
+                }
+
+                int start = Integer.parseInt(tokens[4]);
+                int end = Integer.parseInt(tokens[6]);
+                int length = end - start + 1;
+                raf.seek(start);
+                byte[] fileData = new byte[length];
+                raf.read(fileData);
+                String base64Data = Base64.getEncoder().encodeToString(fileData);
+                String response = String.format("FILE %s OK START %d END %d DATA %s", tokens[1], start, end, base64Data);
+                sendResponse(dataSocket, clientAddress, clientPort, response);
+            }
+            dataSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
